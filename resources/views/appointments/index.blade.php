120eeed2e8069219c978162f6191e36d84782c90
Shaun Gill <shaung75@gmail.com>
2022-09-05 10:52:31 +0100

    Adding complete flag to appointments list

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -51,14 +51,28 @@
 
               @for ($i = 1; $i <= $daysInMonth; $i++)
                 <tr>
-                  <td>{{$i}}</td>
-                  <td colspan="2">
+                  <td class="cell">{{$i}}</td>
+                  <td class="cell">
                     @isset($appointments[$i])
                       @foreach($appointments[$i] as $appointment)
                         {{\Carbon\Carbon::parse($appointment->date)->format('H:i')}} - 
                         <a href="/appointments/{{$appointment->id}}">
                           {{$appointment->client->first_name}} {{$appointment->client->surname}}
                         </a><br>
+                      @endforeach
+                    @endisset
+                  </td>
+                  <td class="cell text-end">
+                    @isset($appointments[$i])
+                      @foreach($appointments[$i] as $appointment)
+                        @if($appointment->complete == 1)
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                          </svg>
+                          <br>
+                        @else
+                          &nbsp;<br>
+                        @endif
                       @endforeach
                     @endisset
                   </td>
